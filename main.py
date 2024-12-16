e9e7fb79984a0bb21896c835ba6eb44b6b7b63f8
ch-iv <icheppy101@gmail.com>
2024-12-15 20:27:19 -0500

    Add curses TUI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import subprocess
 import sys
+import curses
 
 MEMNOTE_SUFFIXES: list[str] = [".memnote", ".txt"]
 EDITOR_CMD = "nvim"
@@ -31,18 +32,40 @@ def create_note(name: str) -> Note:
 
 def main() -> None:
     if len(sys.argv) < 2:
+        stdscr = curses.initscr()
+        stdscr.keypad(True)
+        curses.noecho()
+        curses.cbreak()
+
+        list_start_y = 1
+        list_cursor_y = list_start_y
         while True:
+            height, width = stdscr.getmaxyx()
+            stdscr.addstr(0, 0, "Mem".center(width), curses.A_BOLD)
+            stdscr.move(list_cursor_y, 0)
+
             notes = get_notes_in_dir(NOTES_PATH)
-            if len(notes) == 0:
-                print("There are no notes")
-                return
-            for n, note in enumerate(notes):
-                print(n, note.content_path.name.removesuffix("".join(MEMNOTE_SUFFIXES)))
-            note_to_edit = input("Enter note id: ")
-            if not note_to_edit.isnumeric() and (int(note_to_edit) < 0 or int(note_to_edit) > len(notes) - 1):
-                continue
-            note = notes[int(note_to_edit)]
-            edit_note(note)
+            for i, note in enumerate(notes):
+                note_name = note.content_path.name.removesuffix("".join(MEMNOTE_SUFFIXES))
+                stdscr.addstr(i + list_start_y, 0, note_name.ljust(width),
+                              curses.A_STANDOUT if list_cursor_y - list_start_y == i else 0)
+
+            stdscr.refresh()
+            char = stdscr.getch()
+            if char == curses.KEY_DOWN:
+                list_cursor_y = max(list_start_y, min(list_cursor_y + 1, len(notes)))
+            elif char == curses.KEY_UP:
+                list_cursor_y = max(list_start_y, min(list_cursor_y - 1, len(notes)))
+            elif char == 10:  # enter key
+                note_idx = list_cursor_y - list_start_y
+                # http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/misc.html
+                curses.def_prog_mode()
+                curses.endwin()
+
+                edit_note(notes[note_idx])
+
+                curses.reset_prog_mode()
+                stdscr.refresh()
     else:
         if sys.argv[1].casefold() == "new":
             note_name = input("Name: ")
@@ -53,5 +76,11 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print()
         pass
+    finally:
+        try:
+            curses.nocbreak()
+            curses.echo()
+            curses.endwin()
+        except curses.error:
+            pass
